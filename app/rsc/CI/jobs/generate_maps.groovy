481e8d8373c6f91a5d00c4611533d92192d0fcb0
Jay <james.carson.resume@gmail.com>
2018-08-19 22:15:57 -0500

    CI: James Carson added a base job for all of the map generation.

---
+++ app/rsc/CI/jobs/generate_maps.groovy
@@ -0,0 +1,19 @@
+job('{{DEFAULT_PATH}}/generate-maps-{{MAP_TYPE}}'){
+    scm {
+        git {
+            remote {
+                url({{FUN}})
+            }
+        branch('{{BRANCH}}')
+        }
+    }
+    triggers{
+        scm('*/15 * * * *')
+    }
+    steps{
+        shell('cd {{ARTIFACT_REPO_PATH}}; rm {{MAP_TYPE}}*')
+        shell('cd {{PATH}}/scripts; python {{SCRIPT}}')
+        shell('cp {{PATH}}/PNG/{{MAP_TYPE}}* {{ARTIFACT_REPO_PATH}}')
+        shell('rm {{PATH}}/PNG/{{MAP_TYPE}}*')
+    }
+}
