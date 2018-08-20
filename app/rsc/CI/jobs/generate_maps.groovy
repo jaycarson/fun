cefa95efa68b1b72ca45459902b258167704837b
Jay <james.carson.resume@gmail.com>
2018-08-19 22:22:42 -0500

    CI: James Carson added an 'or true' to a bash command so that it won't error out if there are no files to delete.

--- app/rsc/CI/jobs/generate_maps.groovy
+++ app/rsc/CI/jobs/generate_maps.groovy
@@ -11,7 +11,7 @@ job('{{DEFAULT_PATH}}/generate-maps-{{MAP_TYPE}}'){
         scm('*/15 * * * *')
     }
     steps{
-        shell('cd {{ARTIFACT_REPO_PATH}}; rm {{MAP_TYPE}}*')
+        shell('cd {{ARTIFACT_REPO_PATH}}; rm {{MAP_TYPE}}* || true')
         shell('cd {{PATH}}/scripts; python {{SCRIPT}}')
         shell('cp {{PATH}}/PNG/{{MAP_TYPE}}* {{ARTIFACT_REPO_PATH}}')
         shell('rm {{PATH}}/PNG/{{MAP_TYPE}}*')
